dbc5f2e18f8fa51dd05034e96bf99d339f59f1d5
EduardsLapins <eduards.lapins@inbox.lv>
2021-01-21 23:24:20 +0200

    Character class with some attacking options and the required testing -up for further improvement after lectures

--- src/main/Player/Character.scala
+++ src/main/Player/Character.scala
@@ -4,16 +4,34 @@ class Character (var maxhp: Int, var hp: Int, var attk: Int, var deff: Int, var 
 
   var isAlive: Boolean = true
 
-  def takeDamage(attkStrength: Int): Unit ={
+  def takeDamage(attkStrength: Int): Unit = {
     if (this.hp > 0) {
       this.hp -= attkStrength
-      if (this.hp <= 0){
+      if (this.hp <= 0) {
         isAlive = false
       }
     }
-    else{
+    else {
       isAlive = false
     }
-
   }
+
+  def physAttack(cietejs: Character): Unit ={
+    var saape: Int = (this.attk * (this.attk.toFloat / cietejs.deff.toFloat)).round
+    cietejs.takeDamage(saape)
+  }
+
+  def magAttack(cietejs: Character): Unit ={
+    if(this.mana > 5) {
+      var magSaape: Int = (this.magattk * (this.magattk.toFloat / cietejs.magdeff.toFloat)).round
+      this.mana -= 5
+      cietejs.takeDamage(magSaape)
+    }
+    else if(this.mana > 0) {
+      var magSaape: Int = math.ceil(this.magattk.toFloat * (this.mana.toFloat/5.toFloat) * (this.magattk.toFloat / cietejs.magdeff.toFloat)).toInt
+      this.mana = 0
+      cietejs.takeDamage(magSaape)
+    }
+  }
+
 }
--- src/test/testTesting.scala
+++ src/test/testTesting.scala
@@ -2,14 +2,75 @@ import org.scalatest._
 import Player.Character
 
 class testTesting extends FunSuite{
-  test("a simple test of all current Character functions") {
+  test("a simple test of all takeDamage functions") {
     val Boi: Character = new Character(45,10, 20, 30, 90, 39, 30, 4)
-    assert(Boi.isAlive == true)
+
+    //laimīgs dzīvo
+    assert(Boi.isAlive)
+
+    //uzkrīt lāsteka ->  >;(
     Boi.takeDamage(5)
-    assert(Boi.isAlive == true)
+    assert(Boi.isAlive)
     assert(Boi.hp == 5)
+
+    //uzkrīt flīģelis -> x_x
     Boi.takeDamage(5)
     assert(Boi.hp == 0)
     assert(Boi.isAlive == false)
+  }
+
+  test("testing physAttack") {
+    val Sitejs: Character = new Character(45,10, 20, 30, 10, 39, 30, 30)
+    val Mizejs: Character = new Character(20,20, 10, 50, 10, 15, 30, 4)
+
+    //kaujas
+    assert(Mizejs.isAlive)
+    Sitejs.physAttack(Mizejs)
+    assert(Mizejs.hp == 12)
+    assert(Mizejs.isAlive)
+    Sitejs.physAttack(Mizejs)
+    assert(Mizejs.hp == 4)
+
+    //beidzot paruba
+    Sitejs.physAttack(Mizejs)
+    assert(Mizejs.hp == -4)
+    assert(Mizejs.isAlive == false)
+  }
+
+  test("testing magAttack") {
+    val Sitejs: Character = new Character(45,10, 20, 30, 6, 39, 32, 31)
+    val Mizejs: Character = new Character(20,20, 10, 50, 10, 19, 30, 4)
+
+    //baigi kaujas
+    assert(Mizejs.isAlive)
+    Sitejs.magAttack(Mizejs)
+    assert(Mizejs.hp == 18)
+    assert(Mizejs.isAlive)
+    Sitejs.magAttack(Mizejs)
+    assert(Mizejs.hp == 16)
+    Sitejs.magAttack(Mizejs)
+    assert(Mizejs.hp == 14)
+    assert(Mizejs.isAlive)
+    assert(Sitejs.mana == 16)
+
+    //turpina šaut pupiņas
+    Sitejs.magAttack(Mizejs)
+    Sitejs.magAttack(Mizejs)
+    Sitejs.magAttack(Mizejs)
+    assert(Mizejs.hp == 8)
+    assert(Mizejs.isAlive)
+    assert(Sitejs.mana == 1)
+
+    //turpina šaut pupiņas
+    Sitejs.magAttack(Mizejs)
+    assert(Sitejs.mana == 0)
+    assert(Mizejs.isAlive)
+    assert(Mizejs.hp == 7)
+
+    //vēlreiz pakutina, bet nesanāk
+    Sitejs.magAttack(Mizejs)
+    assert(Sitejs.mana == 0)
+    assert(Mizejs.isAlive)
+    assert(Mizejs.hp == 7)
   }
 }
